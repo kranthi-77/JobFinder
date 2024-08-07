6611da9c2a76a0f0b2bb1906c52dc19d03741a5c
kranthi-77 <vkranthi485@gmail.com>
2024-08-07 17:16:56 +0000

    solution

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,27 +15,27 @@ const Header = props => {
     history.replace('/login')
   }
   return (
-    <nav className="nav-container">
-      <ul className="header-ul-container">
-        <li className="logo-container">
-          <Link className="link" to="/">
-            <img className="logo" src={websiteLogo} alt="website logo" />
+    <nav className='nav-container'>
+      <ul className='header-ul-container'>
+        <li className='logo-container'>
+          <Link className='link' to='/'>
+            <img className='logo' src={websiteLogo} alt='website logo' />
           </Link>
         </li>
-        <li className="home-jobs-container">
-          <Link className="link" to="/">
-            <ImHome className="home-icon" />
-            <h1 className="nav-text">Home</h1>
+        <li className='home-jobs-container'>
+          <Link className='link' to='/'>
+            <ImHome className='home-icon' />
+            <h1 className='nav-text'>Home</h1>
           </Link>
-          <Link className="link" to="/jobs">
-            <h1 className="nav-text">Jobs</h1>
+          <Link className='link' to='/jobs'>
+            <h1 className='nav-text'>Jobs</h1>
 
-            <button className="home-jobs-btn"> Jobs</button>
+            <button className='home-jobs-btn'> Jobs</button>
           </Link>
         </li>
         <li>
-          <FiLogOut className="home-icon" onClick={onClickLogout} />
-          <button type="button" className="btn-logout" onClick={onClickLogout}>
+          <FiLogOut className='home-icon' onClick={onClickLogout} />
+          <button type='button' className='btn-logout' onClick={onClickLogout}>
             Logout
           </button>
         </li>
